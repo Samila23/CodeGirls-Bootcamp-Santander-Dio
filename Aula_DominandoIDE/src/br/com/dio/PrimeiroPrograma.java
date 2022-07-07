47d4dbaebbcb54c4788324d2b04007d28846ddfe
Sâmila Milena Dias de Paula <samila.milenna@gmail.com>
2022-07-07 11:48:45 -0300

    @samila23 [aplicanto conceitos da IDE]

---
+++ Aula_DominandoIDE/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,23 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	/*public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 2;
+		int b = 3;
+		System.out.println("Olá Mundo!"+ (a + b));
+	} */
+	
+	
+	Gato gato = new Gato();
+	Livros livros = new Livros();
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
