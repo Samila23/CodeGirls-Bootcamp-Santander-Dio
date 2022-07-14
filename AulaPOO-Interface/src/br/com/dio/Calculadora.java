5d7574fc6d64802f8e58df28c32f22a2ca32f13f
Sâmila Milena Dias de Paula <samila.milenna@gmail.com>
2022-07-14 19:52:11 -0300

    Aplicando conceitos de Interface em POO com Java

---
+++ AulaPOO-Interface/src/br/com/dio/Calculadora.java
@@ -0,0 +1,27 @@
+package br.com.dio;
+
+public class Calculadora implements OperacaoMatematica{
+
+	@Override
+	public void soma(double operando1, double operando2) {
+		System.out.println("soma" + (operando1 + operando2));
+	}
+
+	@Override
+	public void subtracao(double operando1, double operando2) {
+		System.out.println("Subtracao" + (operando1 - operando2));
+		
+	}
+
+	@Override
+	public void multiplicacao(double operando1, double operando2) {
+		System.out.println("Multiplicacao" + (operando1 * operando2));
+	}
+
+	@Override
+	public void divisao(double operando1, double operando2) {
+		System.out.println("Divisão" + (operando1 / operando2));
+		
+	}
+
+}
---
+++ AulaPOO-Interface/src/br/com/dio/Main.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Calculadora calculadora = new Calculadora();
+		
+		calculadora.soma(2, 2);
+		calculadora.subtracao(2, 2);
+		calculadora.divisao(4, 2);
+		calculadora.multiplicacao(2, 2);
+	}
+
+}
